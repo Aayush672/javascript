0662a3b02eaacb696e1ea66572a5b044c9fb251c
Aayush672 <123322885+Aayush672@users.noreply.github.com>
2024-08-16 18:05:09 +0000

    changes config done

--- basics/test.js
+++ basics/test.js
@@ -1,1 +1,13 @@
 console.log("Aayush")
+const accountId = 14453
+let accountEmail = "agarwalaayush621@gmail.com"
+var accountPswd = "12345"
+accountCity = "Jaipur"
+let accountState
+
+accountEmail = "aag@gmail.com"
+accountPswd = "9876"
+accountCity = "India"
+// accountId = "7654"
+
+console.table([accountId, accountEmail, accountPswd, accountCity, accountState])
